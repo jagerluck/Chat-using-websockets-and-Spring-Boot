56098162dc56b2cf094c95890863bfdd898e2057
Roman <jagerluck@gmail.com>
2020-04-10 15:32:15 +0300

    before deploying

--- src/main/java/com/chat/jchat0/config/WebSocketConfig.java
+++ src/main/java/com/chat/jchat0/config/WebSocketConfig.java
@@ -19,4 +19,5 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         registry.setApplicationDestinationPrefixes("/app");
         registry.enableSimpleBroker("/topic");
     }
+    
 }
